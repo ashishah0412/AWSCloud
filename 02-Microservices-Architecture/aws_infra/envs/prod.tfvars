e5ab368139be5b603660e9982b57aa35a65982ee
Ashish Shah <ashishah0412@gmail.com>
2025-10-31 21:10:38 +0530

    correct variable name and output variable commented

--- 02-Microservices-Architecture/aws_infra/envs/prod.tfvars
+++ 02-Microservices-Architecture/aws_infra/envs/prod.tfvars
@@ -1,7 +1,7 @@
 # -----------------------------
 # Network
 # -----------------------------
-region     = "us-east-1"
+aws_region     = "us-east-1"
 vpc_cidr   = "10.0.0.0/16"
 azs        = ["us-east-1a", "us-east-1b"]
 
--- 02-Microservices-Architecture/aws_infra/modules/rds/outputs.tf
+++ 02-Microservices-Architecture/aws_infra/modules/rds/outputs.tf
@@ -2,9 +2,9 @@ output "db_endpoint" {
   value = aws_db_instance.postgres.endpoint
 }
 
-output "db_reader_endpoint" {
-  value = aws_db_instance.postgres.reader_endpoint
-}
+# output "db_reader_endpoint" {
+#   value = aws_db_instance.postgres.reader_endpoint
+# }
 
 output "rds_sg_id" {
   value = aws_security_group.rds_sg.id
